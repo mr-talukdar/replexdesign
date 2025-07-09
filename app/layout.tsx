3a6ccadd4a706fe85b496e11052d013e64bdd15b
mrtalukdar <buduman209@gmail.com>
2025-07-09 22:55:30 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+    <Analytics/>
     </html>
   );
 }
